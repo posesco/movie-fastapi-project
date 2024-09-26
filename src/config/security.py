093449b5ef04114b772062fb41e158170a98e01b
Jesus David Posada Escobar <posesco@gmail.com>
2024-09-26 07:16:47 +0200

    Add expiration time token jwt

--- src/config/security.py
+++ src/config/security.py
@@ -1,20 +1,26 @@
 from dotenv import load_dotenv
-from jwt import (
-    encode,
-    decode,
-)
+from jwt import encode, decode, ExpiredSignatureError
 import os
+from datetime import datetime, timedelta, timezone
+
 
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 
 
-def create_token(data: dict):
-    token: str = encode(payload=data, key=SECRET_KEY, algorithm=ALGORITHM)
+def create_token(data: dict, expires_in: int = 15):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=expires_in)
+    to_encode.update({"exp": expire})
+    token: str = encode(payload=to_encode, key=SECRET_KEY, algorithm=ALGORITHM)
     return token
 
 
 def validate_token(token: str) -> dict:
-    data: dict = decode(token, key=SECRET_KEY, algorithms=[ALGORITHM])
-    return data
+    try:
+        data: dict = decode(token, key=SECRET_KEY, algorithms=[ALGORITHM])
+        return data
+    except ExpiredSignatureError:
+        print("El Token ha expirado")
+        return None
