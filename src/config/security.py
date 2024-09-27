2c7c73fc51b147b28a5f1339349b7c588db84d5f
Jesus David Posada Escobar <posesco@gmail.com>
2024-09-27 14:32:22 +0200

    Add movie audit logs

--- src/config/security.py
+++ src/config/security.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta, timezone
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 if not SECRET_KEY:
     raise ValueError("El SECRET_KEY no está definido")
